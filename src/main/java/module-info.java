c77d376b0858357d25d9374afb4a69d64729e285
sriyavemuri <vemurisriya@gmail.com>
2024-11-02 21:54:29 -0400

    final submission commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module com.example.project3 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.project3 to javafx.fxml;
-    exports com.example.project3;
     exports com.example.project3.gui;
     opens com.example.project3.gui to javafx.fxml;
+
+    exports com.example.project3.clinicscheduler;
+    exports com.example.project3.util;
 }
